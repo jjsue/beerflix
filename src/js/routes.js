a1facaf325cd885b35bbd6f1a7b89bcbc005d7b4
jjsue <jjsue_95@hotmail.es>
2020-01-19 11:30:51 +0100

    Pintamos de forma basica para comprobar la funcionalidad entre modulos.

---
+++ src/js/routes.js
@@ -0,0 +1,6 @@
+import renderPrincipal from './section.js';
+console.log("archivo routes");
+const beerSect = document.getElementById('beersection')
+console.log(beerSect);
+renderPrincipal(beerSect);
+console.log(beerSect);
---
+++ src/js/section.js
@@ -0,0 +1,42 @@
+export const renderPrincipal = (sectionInFunction) => {
+    sectionInFunction.innerHTML = `
+    <article class="card" style="width: 20rem">
+    <img src="./src/icon/example.jpg" alt="Beer">
+    <h4 class="card-title">BeerName</h4>
+    <h5 class="card-tittle">Descripción:</h5>
+    <p class="card-body">LoremIpsum Dolor Sit Amet.<br>Buena cerveza.</p>
+  </article>
+  <article class="card" style="width: 20rem">
+    <img src="./src/icon/example.jpg" alt="Beer">
+    <h4 class="card-title">BeerName</h4>
+    <h5 class="card-tittle">Descripción:</h5>
+    <p class="card-body">LoremIpsum Dolor Sit Amet.<br>Buena cerveza.</p>
+  </article>
+  <article class="card" style="width: 20rem">
+    <img src="./src/icon/example.jpg" alt="Beer">
+    <h4 class="card-title">BeerName</h4>
+    <h5 class="card-tittle">Descripción:</h5>
+    <p class="card-body">LoremIpsum Dolor Sit Amet.<br>Buena cerveza.</p>
+  </article>
+  <article class="card" style="width: 20rem">
+    <img src="./src/icon/example.jpg" alt="Beer">
+    <h4 class="card-title">BeerName</h4>
+    <h5 class="card-tittle">Descripción:</h5>
+    <p class="card-body">LoremIpsum Dolor Sit Amet.<br>Buena cerveza.</p>
+  </article>
+  <article class="card" style="width: 20rem">
+    <img src="./src/icon/example.jpg" alt="Beer">
+    <h4 class="card-title">BeerName</h4>
+    <h5 class="card-tittle">Descripción:</h5>
+    <p class="card-body">LoremIpsum Dolor Sit Amet.<br>Buena cerveza.</p>
+  </article>
+  <article class="card" style="width: 20rem">
+    <img src="./src/icon/example.jpg" alt="Beer">
+    <h4 class="card-title">BeerName</h4>
+    <h5 class="card-tittle">Descripción:</h5>
+    <p class="card-body">LoremIpsum Dolor Sit Amet.<br>Buena cerveza.</p>
+  </article>
+    `
+    console.log(sectionInFunction);
+}
+export default renderPrincipal;
