332aedf050e4af2e411482f76b714af2358bcc75
jjsue <jjsue_95@hotmail.es>
2020-01-21 17:22:46 +0100

    Busqueda funcionando

--- src/js/API.js
+++ src/js/API.js
@@ -3,32 +3,44 @@ const API_KEY = 'NVJ0DBN-QEXM4CV-HF2D4EQ-DM2VN0W'
 
 //Tomamos el section:
 const beerSect = document.getElementById('beersection') //Cogemos todo el section
+//Tomamos los valores de la busqueda (Si los hubiera):
+var busqueda = sessionStorage.getItem('lastsearch');
+var limite = sessionStorage.getItem('limit');
+if(limite === null){ //Si no hay limite establecido colocamos uno de 10;
+  limite = 10;
+}
+if(busqueda === null){//Si no hay nada en busqueda lo dejamos como una cadena vacía.
+  busqueda = '';
+}
 //Pintamos cuando carga la web:
-const firstResponse = axios({
-  method: 'get',
-  url: 'https://beerflix-api.herokuapp.com/api/v1/beers?limit=10',
-  //responseType: 'application/json',
-   headers: {
-             'X-API-KEY': API_KEY,
-             accept: 'application/json'
-           },
-})
-  .then(function (response) {
-    //console.log(response.data.beers[0]);
-    for (let i=0; i<10; i++){ //Aqui comenzamos a pintar propiamente dicho.
-        if (i === 0){
-            beerSect.innerHTML = renderPrincipal(response.data.beers[i].image,response.data.beers[i].name, response.data.beers[i].description);
-        }else{
-            beerSect.innerHTML = beerSect.innerHTML + renderPrincipal(response.data.beers[i].image,response.data.beers[i].name, response.data.beers[i].description);
-        }
-    }
+  const firstResponse = axios({
+    method: 'get',
+    url: `https://beerflix-api.herokuapp.com/api/v1/beers?search=${busqueda}&limit=${limite}`,
+    //responseType: 'application/json',
+    headers: {
+              'X-API-KEY': API_KEY,
+              accept: 'application/json'
+            },
   })
-  .catch(function (error) {
-    console.log(error);
-    return "Ha habido un error";
-  })
-  .finally(function () {
-    console.log('Finalizada llamada API');
-  });
-
+    .then(function (response) {
+      for (let i=0; i<10; i++){ //Aqui comenzamos a pintar propiamente dicho.
+          if(response.data.beers[i] === undefined){ //Si la busqueda no devuelve nada no bloqueamos la ejecucion.
+            break;
+          }
+          if (i === 0){
+              beerSect.innerHTML = renderPrincipal(response.data.beers[i].image,response.data.beers[i].name, response.data.beers[i].description);
+          }else{
+              beerSect.innerHTML = beerSect.innerHTML + renderPrincipal(response.data.beers[i].image,response.data.beers[i].name, response.data.beers[i].description);
+          }
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      return "Ha habido un error";
+    })
+    .finally(function () {
+      if(beerSect.innerHTML === ''){
+        beerSect.innerHTML = `<h1>Tu busqueda no ha generado resultados :(</h1>`
+      }
+    });
 export default firstResponse;
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,2 +1,12 @@
 //import renderPrincipal from './section.js';
 import firstResponse from './API.js'; //Solo con esto ya ejecuta
+
+const busqueda = window.location.search;
+
+document.getElementById("miFormulario").addEventListener("submit", guardadoSession);
+
+function guardadoSession() {
+    //Extraer los valores y cargarlos a la session storage.
+    sessionStorage.setItem('lastsearch', document.getElementById("busqueda").value);
+    sessionStorage.setItem('limit', document.getElementById("limite").value);
+  }
