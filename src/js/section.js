24fc8e93c846da1a9eecd576147dc0ca04663726
jjsue <jjsue_95@hotmail.es>
2020-01-25 18:18:18 +0100

    Ajustado navbar para que no se salga en dispositivos moviles y header introducido

--- src/js/section.js
+++ src/js/section.js
@@ -1,6 +1,6 @@
 export const renderPrincipal = (img,beerName,description,beerId) => {
   return `
-    <article class="card text-white bg-dark mb-3 card_modificada">
+    <article class="card text-white bg-dark my-3 mx-3 card_modificada">
       <img class="card-img-top img-detail" src="${img}" alt="Beer">
       <a class="card-title card-header" href="./detail.html?id=${beerId}"><h4>${beerName}</h4></a>
       <h5 class="card-tittle ml-3">Descripción:</h5>
